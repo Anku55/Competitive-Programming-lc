9762332a1522fc6127abf3f5f83f423d17448be5
Ankush <Ankushkushwah307@gmail.com>
2026-06-07 14:56:58 +0530

    otimizied the space complexity

--- DynamicProgramming/lc198HouseRobber.cpp
+++ DynamicProgramming/lc198HouseRobber.cpp
@@ -58,3 +58,38 @@ int rob(vector<int>& nums) {
         
     }
 };
+
+############################ Space Optimizations ############
+class Solution {
+public:
+    // int fun(int idx,vector<int>& nums,vector<int>&dp){
+    //     if(idx==0)return nums[0];
+    //     if(idx<0)return 0;
+    //     if(dp[idx]!=-1)return dp[idx];
+    //     int pick=nums[idx]+fun(idx-2,nums,dp);
+    //     int notPick=fun(idx-1,nums,dp);
+
+    //     return dp[idx]= max(pick,notPick);
+
+    // }
+    int rob(vector<int>& nums) {
+
+        int n=nums.size()-1;
+        vector<int>dp(n+1,-1);
+        int prev=nums[0];
+        int prev2=0;
+       
+        for(int i=1;i<=n;i++){
+            int fs=nums[i];
+            if(i>1)fs+=prev2;
+            int ss=prev;
+           int cur=max(fs,ss);
+            prev2=prev;
+            prev=cur;
+        }
+    //   return dp[n];
+
+    return prev;
+        
+    }
+};
