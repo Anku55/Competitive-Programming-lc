f9dc3c7f0fbc6979372157e7017c6c9cd5138b28
Ankush <Ankushkushwah307@gmail.com>
2026-06-14 20:03:11 +0530

    solved max sum without adjacent on cfg

---
+++ DynamicProgramming/MaxSumwithoutAdjacents.cpp
@@ -0,0 +1,21 @@
+###############Memoaization##########
+
+class Solution {
+  public:
+    int fun(int ind,vector<int>& arr, vector<int>&dp ){
+        if(ind==0)return arr[0];
+        if(ind<0)return 0;
+        if(dp[ind]!=-1)return dp[ind];
+        
+        int notpick=fun(ind-1,arr,dp);
+        int pick=arr[ind]+fun(ind-2,arr,dp);
+        return dp[ind]= max(pick,notpick);
+    }
+    // calculate the maximum sum with out adjacent
+    int findMaxSum(vector<int>& arr, int n) {
+        vector<int>dp(n,-1);
+        return fun(n-1,arr,dp);
+        // code here
+        
+    }
+};
