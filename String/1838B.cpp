a3e1f477ce561a1846d6deccddc9eb812388547c
Ankush <Ankushkushwah307@gmail.com>
2026-06-15 19:11:21 +0530

    solved 1838B.cpp on cf

---
+++ String/1838B.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+
+    string s;
+    cin >> s;
+
+    vector<int> freq(26, 0);
+
+    for (char c : s) {
+        freq[c - 'a']++;
+    }
+
+    int odd = 0;
+
+    for (int i = 0; i < 26; i++) {
+        if (freq[i] % 2) {
+            odd++;
+        }
+    }
+
+    if (odd <= k + 1) {
+        cout << "YES\n";
+    } else {
+        cout << "NO\n";
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
