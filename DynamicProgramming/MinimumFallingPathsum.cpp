3130a4d2a520ca2de206b73613ba4fc8135e4bb6
Ankush <Ankushkushwah307@gmail.com>
2026-06-16 20:14:39 +0530

    improve the space c omplexity

--- DynamicProgramming/MinimumFallingPathsum.cpp
+++ DynamicProgramming/MinimumFallingPathsum.cpp
@@ -76,3 +76,36 @@ public:
         
     }
 };
+
+##############  Tabulation #########
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = matrix[0][j];
+        }
+
+        int ans = 1e9;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                int s = matrix[i][j] +dp[i-1][j];
+                int ld=1e9;
+                if(j-1>=0)ld = matrix[i][j] + dp[i-1][j-1];
+                int rd = 1e9;
+                if(j+1<n)rd=matrix[i][j] + dp[i-1][j+1];
+
+                dp[i][j]=min(s,min(ld,rd));
+            }
+        }
+
+        int mini=dp[n-1][0];
+        for(int j=1;j<n;j++){
+            mini=min(mini,dp[n-1][j]);
+        }
+
+        return mini;
+    }
+};
