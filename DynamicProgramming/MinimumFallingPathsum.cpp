13ef1fcb07508deab6a250ba95587027773b9bcd
Ankush <Ankushkushwah307@gmail.com>
2026-06-16 20:13:26 +0530

    applied memoaization

--- DynamicProgramming/MinimumFallingPathsum.cpp
+++ DynamicProgramming/MinimumFallingPathsum.cpp
@@ -42,3 +42,37 @@ public:
         
     }
 };
+
+optimize the time complexity 
+############Memoaization
+
+class Solution {
+public:
+    int fun (int i,int j,vector<vector<int>>& matrix, vector<vector<int>>&dp){
+        if(i < 0 || j < 0 || j >= matrix.size()){
+    return 1e9;
+}
+        if(i==0){
+            return matrix[0][j];
+        }
+
+        int s=matrix[i][j]+fun(i-1,j,matrix,dp);
+        int ld=matrix[i][j]+fun(i-1,j-1,matrix,dp);
+        int rd=matrix[i][j]+fun(i-1,j+1,matrix,dp);
+
+        return min(s,min(ld,rd));
+
+    }
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        
+        int ans=1e9;
+        for(int j = 0; j < n; j++){
+        ans = min(ans, fun(n-1, j, matrix,dp));
+    }
+
+    return ans;
+        
+    }
+};
