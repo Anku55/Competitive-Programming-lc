54977297b182d905e3b0fca5706022624720b5f4
Ankush <Ankushkushwah307@gmail.com>
2026-06-18 22:50:33 +0530

    tabulation way

--- DynamicProgramming/longestCommonSubsequence.cpp
+++ DynamicProgramming/longestCommonSubsequence.cpp
@@ -50,3 +50,35 @@ public:
         
     }
 };
+
+############## Tabulation ##########
+
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.length();
+        int m = text2.length();
+        string s = text1;
+        string l = text2;
+
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i = 0; i < m; i++) {
+            dp[0][i] = 0;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s[i - 1] == l[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+};
