45ae92619f49070dda59f08decf512f7b63899f9
Ankush <Ankushkushwah307@gmail.com>
2026-06-21 11:09:49 +0530

    quetion no 1 of 507 contes

---
+++ CONTEST/LEETCODE/Weekly Contest 507/Ques01.cpp
@@ -0,0 +1,26 @@
+// The Manhattan Distance between two cells 
+// (xi, yi) and (xj, yj) is |xi - xj| + |yi - yj|
+// there is a property of manhattam distance the order of 
+//opration doesnt matter after performing LLRlRURUDUDD
+// or you perfor LLLRRRDDDUUU
+
+
+
+#define ll long long
+class Solution {
+public:
+    int maxDistance(string moves) {
+        ll x=0;
+        ll y=0;
+        ll k=0;
+        
+        for(auto & ch : moves){
+            if(ch=='R')x++;
+            else if(ch=='L')x--;
+            else if(ch=='U')y++;
+            else if(ch=='D')y--;
+            else k++;
+        }
+        return abs(x)+abs(y)+k;
+    }
+};
