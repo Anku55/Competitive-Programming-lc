1b96ee7d9e97fd1a48087dcfd6008cf685c4b7ba
Ankush <Ankushkushwah307@gmail.com>
2026-06-30 22:28:50 +0530

    Graph Bipartite?

---
+++ Graph/Bipartite.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool fun(int node, int col, vector<vector<int>>& graph, vector<int>& color) {
+        color[node] = col;
+
+        for (auto it : graph[node]) {
+            if (color[it] == -1) {
+                if (!fun(it, !col, graph, color))
+                    return false;
+            }
+            else if (color[it] == color[node]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> color(n, -1);
+
+        for (int i = 0; i < n; i++) {
+            if (color[i] == -1) {
+                if (!fun(i, 0, graph, color))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+};
