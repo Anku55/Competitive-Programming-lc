9c84c7a77c4f2fc6ade9c9f76ec7937522fb95f1
Ankush <Ankushkushwah307@gmail.com>
2026-07-03 12:56:47 +0530

    implemented heap and heapfy algorithem

--- Heap/heap.cpp
+++ Heap/heap.cpp
@@ -1,5 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
+// in 1-based indexing
+// leftChind=2*i;
+//rightChild=2*i+1;
+//leafnode n/2+1 --- n;
+// 0 based indexing
+// left child =2*i+1;
+// rightChind =2*i+2;
 
 class heap
 {
@@ -34,6 +41,36 @@ class heap
             }
         }
     }
+    void deleteFromHeap(){
+        // deletind root 
+        if(size==0){
+            cout<<"Nothing to delete"<<endl;
+            return;
+        }
+
+        arr[1]=arr[size];
+        size--;
+        int i=1;
+        // take root one to its right position
+
+        while(i<size){
+            int leftIndex=2*i;
+            int rightIndex=2*i+1;
+            if(leftIndex< size && arr[i]<arr[leftIndex]){
+                swap(arr[leftIndex],arr[i]);
+                i=leftIndex;
+
+            }else if(rightIndex<size&&arr[rightIndex]<arr[i]){
+                swap(arr[i],arr[rightIndex]);
+                i=rightIndex;
+            }else{
+                break;
+            }
+        }
+    }
+
+
+
     void print(){
         for(int i=1;i<=size;i++){
             cout<<arr[i]<<" ";
@@ -41,14 +78,51 @@ class heap
     }
 };
 
+    void heapyFy(int arr[],int n ,int i){
+
+        int largest=i;
+        int leftIndex=2*i;
+        int rightIndex=2*i+1;
+
+        if(leftIndex<n && arr[largest]<arr[leftIndex]){
+            largest=leftIndex;
+        }
+        if(rightIndex<n && arr[largest]<arr[rightIndex]){
+            largest=rightIndex;
+        }
+
+        if(largest !=i){
+            swap(arr[largest],arr[i]);
+            heapyFy(arr,n,largest);
+
+        }
+    }
+
 int main(){
 
     heap h;
+
     h.insertInHeap(10);
     h.insertInHeap(30);
     h.insertInHeap(25);
     h.insertInHeap(45);
 
     h.print();
+    h.deleteFromHeap();
+    h.print();
+
+    int arr[6]={-1,4,5,6,7,8};
+    int n=6;
+    for(int i=n/2;i>0;i--){
+        heapyFy(arr,n,i);
+
+    }
+    cout<<"printing new Array"<<endl;
+
+    for(int i=1;i<n;i++){
+        cout<<arr[i]<<" ";
+       
+        
+    }cout<<endl;
 
 }
