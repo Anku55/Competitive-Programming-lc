50a75f8f8a1bb9b3c42b0b04d732d24ecd064a7b
Ankush <Ankushkushwah307@gmail.com>
2026-07-05 17:31:23 +0530

    CheapestPrice k flight

---
+++ Graph/cheapestflight.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights,
+                          int src, int dst, int k) {
+
+        vector<pair<int, int>> adj[n];
+
+        for (auto &it : flights) {
+            adj[it[0]].push_back({it[1], it[2]});
+        }
+
+        vector<int> dist(n, INT_MAX);
+
+        queue<pair<int, pair<int, int>>> q;
+        // {stops, {node, cost}}
+
+        q.push({0, {src, 0}});
+        dist[src] = 0;
+
+        while (!q.empty()) {
+
+            auto it = q.front();
+            q.pop();
+
+            int stops = it.first;
+            int node = it.second.first;
+            int cost = it.second.second;
+
+            if (stops > k)
+                continue;
+
+            for (auto &nbr : adj[node]) {
+
+                int adjNode = nbr.first;
+                int edgeCost = nbr.second;
+
+                if (cost + edgeCost < dist[adjNode]) {
+
+                    dist[adjNode] = cost + edgeCost;
+
+                    q.push({stops + 1, {adjNode, dist[adjNode]}});
+                }
+            }
+        }
+
+        if (dist[dst] == INT_MAX)
+            return -1;
+
+        return dist[dst];
+    }
+};
