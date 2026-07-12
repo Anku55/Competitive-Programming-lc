9446c0626b08fafad999605f97a52dd2f8ea293c
Ankush <Ankushkushwah307@gmail.com>
2026-07-12 21:51:39 +0530

    prims algo

--- Graph/primsAlgo.cpp
+++ Graph/primsAlgo.cpp
@@ -32,6 +32,38 @@ int primMST(int V, vector<vector<pii>> &adj) {
 
     return mstWeight;
 }
+/// if graph is disconnected
+int primMST(int V, vector<vector<pii>> &adj) {
+    priority_queue<pii, vector<pii>, greater<pii>> pq;
+    vector<int> vis(V, 0);
+    int mstWeight = 0;
+
+    for (int start = 0; start < V; start++) {
+        if (vis[start])
+            continue;
+
+        pq.push({0, start});
+
+        while (!pq.empty()) {
+            auto [wt, node] = pq.top();
+            pq.pop();
+
+            if (vis[node])
+                continue;
+
+            vis[node] = 1;
+            mstWeight += wt;
+
+            for (auto &[adjNode, edgeWt] : adj[node]) {
+                if (!vis[adjNode]) {
+                    pq.push({edgeWt, adjNode});
+                }
+            }
+        }
+    }
+
+    return mstWeight;
+}
 
 int main() {
     int V, E;
