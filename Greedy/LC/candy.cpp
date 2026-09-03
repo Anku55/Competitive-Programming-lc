396ac5878e2a7698f2ef825665c62463c3c4865e
Ankush <Ankushkushwah307@gmail.com>
2026-09-03 23:56:55 +0530

    candy

---
+++ Greedy/LC/candy.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n=ratings.size();
+        int sum=1;
+        int i=1;
+        while(i<n){
+            if(ratings[i]==ratings[i-1]){
+                sum+=1;
+                i++;
+                continue;
+            }
+
+            int peak=1;
+            while(i<n&&ratings[i-1]<ratings[i]){
+                peak+=1;
+                sum+=peak;
+                i++;
+            }
+            int down=1;
+            while(i<n&&ratings[i]<ratings[i-1]){
+                sum+=down;
+                i++;
+                down+=1;
+            }
+            if(down>peak){
+                sum+=down-peak;
+            }
+        }
+        return sum;
+        
+    }
+};
